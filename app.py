ca29575dd4767ae06ea23b38588a5203e2582abd
Devbrat Singh <devbrat.singh@siemens.com>
2020-04-02 18:43:13 +0530

    requirements file updated

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ def status():
 
 @app.route('/test')
 def getData():
-    return "test"
+    data=pd.read_csv("HearCareHealthData.csv")
+    x = data.to_json()
+    return x
 
 
 @app.route('/analyze', methods=['GET'])                  
