2f2b5dbf2b43c91a561bea8f5e9d806708ced2dd
Devbrat Singh <devvbrat1107@gmail.com>
2020-06-12 17:06:00 +0530

    blob changes

--- DataAnalyzer.py
+++ DataAnalyzer.py
@@ -4,7 +4,6 @@ import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from matplotlib import pyplot as plt
 
 
 class MlModel:
