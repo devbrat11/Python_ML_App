88e860c89b261e086e0e4d201278296c71ea8c7f
Devbrat Singh <32700744+devbrat11@users.noreply.github.com>
2020-07-19 21:41:44 +0530

    Update DataStore.py

--- DataStore.py
+++ DataStore.py
@@ -2,7 +2,6 @@ from azure.storage.blob import BlobServiceClient
 
 class BlobDataReader:
     CONNECTIONSTRING = 'DefaultEndpointsProtocol=https;AccountName=mlappdata;AccountKey=LiE9PRgvx0vl6HPkK9Kapw1lG71Ge7Gvi/gULY3bqk+v7jqO6E+Xjg/oOOA+eArGWYD4TWhN9p0I8qGlvNPY6Q==;EndpointSuffix=core.windows.net'
-    DEST_FILE = 'Test.csv'
     CONTAINERNAME = 'datasets'
     FILENAME = 'heart.csv'
 
